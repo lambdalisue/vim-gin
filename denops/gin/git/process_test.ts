32e4f7998d4c72b48ac69e2780194444fdfd4404
Alisue <lambdalisue@hashnote.net>
2021-06-29 02:01:00 +0900

    :+1: Add `git/process.ts` module to run/execute `git` command

---
+++ denops/gin/git/process_test.ts
@@ -0,0 +1,41 @@
+import { assert, assertThrowsAsync } from "../deps_test.ts";
+import { decodeUtf8 } from "../text.ts";
+import { execute, ExecuteError, run } from "./process.ts";
+
+Deno.test("run() runs 'git' and return a process", async () => {
+  const proc = run(["version"], {
+    stdout: "piped",
+  });
+  const [status, stdout] = await Promise.all([
+    proc.status(),
+    proc.output(),
+  ]);
+  proc.close();
+  assert(status.success);
+  assert(decodeUtf8(stdout).startsWith("git version"));
+});
+
+Deno.test("run() runs 'git' and return a process (noOptionalLocks)", async () => {
+  const proc = run(["version"], {
+    stdout: "piped",
+    noOptionalLocks: true,
+  });
+  const [status, stdout] = await Promise.all([
+    proc.status(),
+    proc.output(),
+  ]);
+  proc.close();
+  assert(status.success);
+  assert(decodeUtf8(stdout).startsWith("git version"));
+});
+
+Deno.test("execute() runs 'git' and return a stdout on success", async () => {
+  const stdout = await execute(["version"]);
+  assert(decodeUtf8(stdout).startsWith("git version"));
+});
+
+Deno.test("execute() runs 'git' and throw ExecuteError on fail", async () => {
+  await assertThrowsAsync(async () => {
+    await execute(["no-such-command"]);
+  }, ExecuteError);
+});
