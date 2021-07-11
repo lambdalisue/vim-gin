e69f0844590dbcde2a2a89c7437c03fcdc32fd3d
Alisue <lambdalisue@hashnote.net>
2021-07-11 23:52:27 +0900

    :+1: Add `core/cmd` module to handle Vim's command arguments

--- denops/gin/deps.ts
+++ denops/gin/deps.ts
@@ -1,3 +1,9 @@
 export { Cache } from "https://deno.land/x/local_cache@1.0/mod.ts";
 
+export type { Denops } from "https://deno.land/x/denops_std@v1.8.1/mod.ts";
+export * as batch from "https://deno.land/x/denops_std@v1.8.1/batch/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v1.8.1/function/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v1.8.1/helper/mod.ts";
+
 export * as path from "https://deno.land/std@0.106.0/path/mod.ts";
+export { deadline } from "https://deno.land/std@0.106.0/async/mod.ts";
