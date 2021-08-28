fadc9fdaf6bfb9d7166f1a355eeb141241527acf
Alisue <lambdalisue@hashnote.net>
2021-08-28 22:53:48 +0900

    :bug: Fix behavior of `Gin` command on error

--- denops/gin/feature/native/command.ts
+++ denops/gin/feature/native/command.ts
@@ -28,11 +28,12 @@ export async function command(
     await denops.cmd("echohl Error");
     await helper.echo(denops, decodeUtf8(stderr));
     await denops.cmd("echohl None");
+  } else {
+    await helper.echo(denops, decodeUtf8(stdout) + decodeUtf8(stderr));
+    await autocmd.emit(denops, "User", "GinNativeCommandPost", {
+      nomodeline: true,
+    });
   }
-  await helper.echo(denops, decodeUtf8(stdout) + decodeUtf8(stderr));
-  await autocmd.emit(denops, "User", "GinNativeCommandPost", {
-    nomodeline: true,
-  });
 }
 
 export async function bind(denops: Denops, bufnr: number): Promise<void> {
