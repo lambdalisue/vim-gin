14e8ddf210fd9f86104214a1d0af3f6a1cea4073
Alisue <lambdalisue@hashnote.net>
2021-12-30 14:40:48 +0900

    :+1: Use plugin to define entrypoints of `reload`

--- denops/gin/feature/reload/main.ts
+++ denops/gin/feature/reload/main.ts
@@ -1,20 +1,12 @@
-import { batch, Denops, unknownutil } from "../../deps.ts";
+import { Denops, unknownutil } from "../../deps.ts";
 import { command } from "./command.ts";
 
 export async function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
     ...denops.dispatcher,
-    "reload:command": async (bufnr) => {
-      if (bufnr == null) {
-        bufnr = await denops.call('bufnr', '%');
-      }
+    "reload:command": (bufnr) => {
       unknownutil.ensureNumber(bufnr);
-      return await command(denops, bufnr);
+      return command(denops, bufnr);
     },
   };
-  await batch.batch(denops, async (denops) => {
-    await denops.cmd(
-      `command! -bar -nargs=? GinReload call denops#request('${denops.name}', 'reload:command', [<f-args>])`,
-    );
-  });
 }
