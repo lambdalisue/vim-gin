b2047bc2aa9164a193d6837fb604b97cfe03af3e
Alisue <lambdalisue@hashnote.net>
2022-01-03 17:42:55 +0900

    :+1: Add `--worktree` option to specify worktree on `GinStatus`

--- denops/gin/feature/status/command.ts
+++ denops/gin/feature/status/command.ts
@@ -26,8 +26,13 @@ export async function command(
     },
     "--": true,
   });
-  const cwd = (opts._[0] ?? await fn.getcwd(denops, 0)) as string;
-  const worktree = await find(cwd);
+  let worktree: string;
+  if (opts.worktree) {
+    worktree = opts.worktree;
+  } else {
+    const cwd = await fn.getcwd(denops) as string;
+    worktree = await find(cwd);
+  }
   const bname = bufname.format({
     scheme: "ginstatus",
     expr: worktree,
