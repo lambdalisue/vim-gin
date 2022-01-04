b0c0b21fc70ac759c1f56b83530ea1e10cddc333
Alisue <lambdalisue@hashnote.net>
2022-01-04 17:54:31 +0900

    :+1: Add `expand()` function to expand Gin's file-like buffer

--- denops/gin/util/cmd.ts
+++ denops/gin/util/cmd.ts
@@ -1,5 +1,10 @@
-import { Denops, flags, fn } from "../deps.ts";
+import { bufname, Denops, flags, fn } from "../deps.ts";
 import { find } from "../git/finder.ts";
+
+const GIN_FILE_BUFFER_PROTOCOLS = [
+  "gindiff://",
+  "ginshow://",
+];
 
 export async function normCmdArgs(
   denops: Denops,
@@ -19,13 +24,25 @@ async function normCmdArg(
     return await cache.get(arg)!;
   }
   if (arg.startsWith("%") || arg.startsWith("#")) {
-    const p = denops.call("gin#internal#util#cmd#expand", arg) as Promise<
-      string
-    >;
+    const p = expand(denops, arg);
     cache.set(arg, p);
     return await p;
   }
   return arg.replaceAll(/^\\(%|#)/g, "$1");
+}
+
+export async function expand(denops: Denops, expr: string): Promise<string> {
+  const bname = await denops.call(
+    "gin#internal#util#cmd#expand",
+    expr,
+  ) as string;
+  for (const protocol of GIN_FILE_BUFFER_PROTOCOLS) {
+    if (bname.startsWith(protocol)) {
+      const { fragment } = bufname.parse(bname);
+      return fragment ?? bname;
+    }
+  }
+  return bname;
 }
 
 export async function getOrFindWorktree(
