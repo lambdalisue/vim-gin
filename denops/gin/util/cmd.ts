34d92d60098cf1718419f4dba1256e6b04d8a6c9
Alisue <lambdalisue@hashnote.net>
2022-01-04 14:02:15 +0900

    :shower: Remove treeish related codes

--- denops/gin/util/cmd.ts
+++ denops/gin/util/cmd.ts
@@ -1,5 +1,4 @@
 import { Denops, flags, fn } from "../deps.ts";
-import { parseTreeish } from "../git/treeish.ts";
 import { find } from "../git/finder.ts";
 
 export async function normCmdArgs(
@@ -15,7 +14,6 @@ async function normCmdArg(
   denops: Denops,
   arg: string,
   cache: Map<string, Promise<string>>,
-  nested = false,
 ): Promise<string> {
   if (cache.has(arg)) {
     return await cache.get(arg)!;
@@ -26,12 +24,6 @@ async function normCmdArg(
     >;
     cache.set(arg, p);
     return await p;
-  }
-  if (!nested) {
-    const [commitish, path] = parseTreeish(arg);
-    if (path) {
-      arg = `${commitish}:${await normCmdArg(denops, path, cache, true)}`;
-    }
   }
   return arg.replaceAll(/^\\(%|#)/g, "$1");
 }
