b482c4c291f52a365b4ccb01c62280b191c8dfb6
Alisue <lambdalisue@hashnote.net>
2022-01-04 19:37:04 +0900

    :+1: Add `gin#util#expand()`

--- autoload/gin/util.vim
+++ autoload/gin/util.vim
@@ -3,3 +3,8 @@ function! gin#util#reload(...) abort
   call denops#plugin#wait('gin')
   call denops#request('gin', 'util:reload', [bufnr])
 endfunction
+
+function! gin#util#expand(expr) abort
+  call denops#plugin#wait('gin')
+  return denops#request('gin', 'util:expand', [a:expr])
+endfunction
--- denops/gin/core/util/main.ts
+++ denops/gin/core/util/main.ts
@@ -1,5 +1,6 @@
 import { Denops, unknownutil } from "../../deps.ts";
 import * as buffer from "../../util/buffer.ts";
+import { expand } from "../../util/cmd.ts";
 
 export function main(denops: Denops): void {
   denops.dispatcher = {
@@ -7,6 +8,11 @@ export function main(denops: Denops): void {
     "util:reload": (bufnr) => {
       unknownutil.ensureNumber(bufnr);
       return buffer.reload(denops, bufnr);
+    },
+
+    "util:expand": (expr) => {
+      unknownutil.ensureString(expr);
+      return expand(denops, expr);
     },
   };
 }
