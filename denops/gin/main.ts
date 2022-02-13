952f2b1ae26ae232f11cd80ab1b85afbd60b3c8f
Alisue <lambdalisue@gmail.com>
2022-02-13 17:28:36 +0900

    :shower: Remove `GinDiscard` and related actions

--- denops/gin/main.ts
+++ denops/gin/main.ts
@@ -6,7 +6,6 @@ import { main as mainDebug } from "./core/debug/main.ts";
 import { main as mainUtil } from "./core/util/main.ts";
 
 import { main as mainDiff } from "./feat/diff/main.ts";
-import { main as mainDiscard } from "./feat/discard/main.ts";
 import { main as mainEdit } from "./feat/edit/main.ts";
 import { main as mainPatch } from "./feat/patch/main.ts";
 import { main as mainStatus } from "./feat/status/main.ts";
@@ -18,7 +17,6 @@ export function main(denops: Denops): void {
   mainUtil(denops);
 
   mainDiff(denops);
-  mainDiscard(denops);
   mainEdit(denops);
   mainPatch(denops);
   mainStatus(denops);
