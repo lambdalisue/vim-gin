0e7e0d51b154a0323da1cc87bcfd1ffc55bf83bc
Alisue <lambdalisue@gmail.com>
2022-02-17 09:35:36 +0900

    :muscle: Extract initialization codes

--- denops/gin/feat/patch/command.ts
+++ denops/gin/feat/patch/command.ts
@@ -65,32 +65,116 @@ export async function command(
 
   // HEAD
   if (bufnrHead !== -1) {
-    await buffer.ensure(denops, bufnrHead, async () => {
-      await batch.batch(denops, async (denops) => {
-        await mapping.map(
-          denops,
-          "<Plug>(gin-diffput)",
-          `<Cmd>diffput ${bufnrIndex}<CR><Cmd>diffupdate<CR>`,
-          {
-            buffer: true,
-            noremap: true,
-          },
-        );
-        await mapping.map(
-          denops,
-          "dp",
-          "<Plug>(gin-diffput)",
-          {
-            buffer: true,
-          },
-        );
-        await denops.cmd("diffthis");
-      });
-    });
+    await initHead(denops, bufnrHead, bufnrIndex);
   }
 
   // INDEX
-  await buffer.ensure(denops, bufnrIndex, async () => {
+  await initIndex(denops, bufnrIndex, bufnrHead, bufnrWorktree);
+
+  // WORKTREE
+  if (bufnrWorktree !== -1) {
+    await initWorktree(denops, bufnrWorktree, bufnrIndex);
+  }
+
+  // Focus INDEX
+  const winid = await fn.bufwinid(denops, bufnrIndex);
+  await fn.win_gotoid(denops, winid);
+}
+
+function parseResidue(
+  residue: string[],
+): [string] {
+  // GinPatch [{options}] {path}
+  switch (residue.length) {
+    case 1:
+      return [residue[0]];
+    default:
+      throw new Error("Invalid number of arguments");
+  }
+}
+
+async function initHead(
+  denops: Denops,
+  bufnr: number,
+  bufnrIndex: number,
+): Promise<void> {
+  await buffer.ensure(denops, bufnr, async () => {
+    await batch.batch(denops, async (denops) => {
+      await mapping.map(
+        denops,
+        "<Plug>(gin-diffput)",
+        `<Cmd>diffput ${bufnrIndex}<CR><Cmd>diffupdate<CR>`,
+        {
+          buffer: true,
+          noremap: true,
+        },
+      );
+      await mapping.map(
+        denops,
+        "dp",
+        "<Plug>(gin-diffput)",
+        {
+          buffer: true,
+        },
+      );
+      await denops.cmd("diffthis");
+    });
+  });
+}
+
+async function initWorktree(
+  denops: Denops,
+  bufnr: number,
+  bufnrIndex: number,
+): Promise<void> {
+  await buffer.ensure(denops, bufnr, async () => {
+    await batch.batch(denops, async (denops) => {
+      await mapping.map(
+        denops,
+        "<Plug>(gin-diffput)",
+        `<Cmd>diffput ${bufnrIndex}<CR><Cmd>diffupdate<CR>`,
+        {
+          buffer: true,
+          noremap: true,
+        },
+      );
+      await mapping.map(
+        denops,
+        "<Plug>(gin-diffget)",
+        `<Cmd>diffget ${bufnrIndex}<CR><Cmd>diffupdate<CR>`,
+        {
+          buffer: true,
+          noremap: true,
+        },
+      );
+      await mapping.map(
+        denops,
+        "dp",
+        "<Plug>(gin-diffput)",
+        {
+          buffer: true,
+        },
+      );
+      await mapping.map(
+        denops,
+        "do",
+        "<Plug>(gin-diffget)",
+        {
+          buffer: true,
+        },
+      );
+      await denops.cmd("diffthis");
+    });
+  });
+}
+
+async function initIndex(
+  denops: Denops,
+  bufnr: number,
+  bufnrHead: number,
+  bufnrWorktree: number,
+): Promise<void> {
+  await buffer.ensure(denops, bufnr, async () => {
     await batch.batch(denops, async (denops) => {
       if (bufnrHead !== -1) {
         await mapping.map(
@@ -182,63 +266,4 @@ export async function command(
       await denops.cmd("diffthis");
     });
   });
-
-  // WORKTREE
-  if (bufnrWorktree !== -1) {
-    await buffer.ensure(denops, bufnrWorktree, async () => {
-      await batch.batch(denops, async (denops) => {
-        await mapping.map(
-          denops,
-          "<Plug>(gin-diffput)",
-          `<Cmd>diffput ${bufnrIndex}<CR><Cmd>diffupdate<CR>`,
-          {
-            buffer: true,
-            noremap: true,
-          },
-        );
-        await mapping.map(
-          denops,
-          "<Plug>(gin-diffget)",
-          `<Cmd>diffget ${bufnrIndex}<CR><Cmd>diffupdate<CR>`,
-          {
-            buffer: true,
-            noremap: true,
-          },
-        );
-        await mapping.map(
-          denops,
-          "dp",
-          "<Plug>(gin-diffput)",
-          {
-            buffer: true,
-          },
-        );
-        await mapping.map(
-          denops,
-          "do",
-          "<Plug>(gin-diffget)",
-          {
-            buffer: true,
-          },
-        );
-        await denops.cmd("diffthis");
-      });
-    });
-  }
-
-  // Focus INDEX
-  const winid = await fn.bufwinid(denops, bufnrIndex);
-  await fn.win_gotoid(denops, winid);
-}
-
-function parseResidue(
-  residue: string[],
-): [string] {
-  // GinPatch [{options}] {path}
-  switch (residue.length) {
-    case 1:
-      return [residue[0]];
-    default:
-      throw new Error("Invalid number of arguments");
-  }
 }
