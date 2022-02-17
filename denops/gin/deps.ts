0eaef8408000a3c4f5d5585ff23d70bff44e3302
GitHub <noreply@github.com>
2022-02-17 00:51:28 +0000

    :package: Update deno dependencies

--- denops/gin/deps.ts
+++ denops/gin/deps.ts
@@ -1,18 +1,18 @@
 export { Cache } from "https://deno.land/x/local_cache@1.0/mod.ts";
 
-export type { Denops } from "https://deno.land/x/denops_std@v2.4.0/mod.ts";
-export * as anonymous from "https://deno.land/x/denops_std@v2.4.0/anonymous/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v2.4.0/autocmd/mod.ts";
-export * as batch from "https://deno.land/x/denops_std@v2.4.0/batch/mod.ts";
-export * as bufname from "https://deno.land/x/denops_std@v2.4.0/bufname/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v2.4.0/function/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.0.0/mod.ts";
+export * as anonymous from "https://deno.land/x/denops_std@v3.0.0/anonymous/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.0.0/autocmd/mod.ts";
+export * as batch from "https://deno.land/x/denops_std@v3.0.0/batch/mod.ts";
+export * as bufname from "https://deno.land/x/denops_std@v3.0.0/bufname/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.0.0/function/mod.ts";
 export * as helper from "./@lib/helper.ts";
-export * as mapping from "https://deno.land/x/denops_std@v2.4.0/mapping/mod.ts";
-export * as option from "https://deno.land/x/denops_std@v2.4.0/option/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v2.4.0/variable/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v3.0.0/mapping/mod.ts";
+export * as option from "https://deno.land/x/denops_std@v3.0.0/option/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.0.0/variable/mod.ts";
 
 export * as unknownutil from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
 
-export * as fs from "https://deno.land/std@0.119.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.119.0/path/mod.ts";
-export { deadline } from "https://deno.land/std@0.119.0/async/mod.ts";
+export * as fs from "https://deno.land/std@0.126.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.126.0/path/mod.ts";
+export { deadline } from "https://deno.land/std@0.126.0/async/mod.ts";
