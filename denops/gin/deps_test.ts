54e73e523d47fc0b59b613fa54cf9562b900032b
Alisue <lambdalisue@gmail.com>
2022-02-19 01:41:18 +0900

    :package: Update denops_std to v3.1.0

--- denops/gin/deps_test.ts
+++ denops/gin/deps_test.ts
@@ -5,4 +5,4 @@ export { using } from "https://deno.land/x/disposable@v1.0.2/mod.ts";
 
 export { sandbox } from "https://deno.land/x/sandbox@v1.0.3/mod.ts";
 
-export * from "https://deno.land/x/denops_std@v3.0.0/test/mod.ts";
+export * from "https://deno.land/x/denops_std@v3.1.0/test/mod.ts";
