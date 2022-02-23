1e98035c4d5c599679e291ea90b4aa2e7e786a0e
Alisue <lambdalisue@gmail.com>
2022-02-24 00:06:43 +0900

    :package: Update denops-std
    Close #10

--- denops/gin/deps.ts
+++ denops/gin/deps.ts
@@ -1,15 +1,15 @@
 export { Cache } from "https://deno.land/x/local_cache@1.0/mod.ts";
 
-export type { Denops } from "https://deno.land/x/denops_std@v3.1.0/mod.ts";
-export * as anonymous from "https://deno.land/x/denops_std@v3.1.0/anonymous/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.1.0/autocmd/mod.ts";
-export * as batch from "https://deno.land/x/denops_std@v3.1.0/batch/mod.ts";
-export * as bufname from "https://deno.land/x/denops_std@v3.1.0/bufname/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.1.0/function/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v3.1.0/helper/mod.ts";
-export * as mapping from "https://deno.land/x/denops_std@v3.1.0/mapping/mod.ts";
-export * as option from "https://deno.land/x/denops_std@v3.1.0/option/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.1.0/variable/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.1.3/mod.ts";
+export * as anonymous from "https://deno.land/x/denops_std@v3.1.3/anonymous/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.1.3/autocmd/mod.ts";
+export * as batch from "https://deno.land/x/denops_std@v3.1.3/batch/mod.ts";
+export * as bufname from "https://deno.land/x/denops_std@v3.1.3/bufname/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.1.3/function/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v3.1.3/helper/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v3.1.3/mapping/mod.ts";
+export * as option from "https://deno.land/x/denops_std@v3.1.3/option/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.1.3/variable/mod.ts";
 
 export * as unknownutil from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
 
