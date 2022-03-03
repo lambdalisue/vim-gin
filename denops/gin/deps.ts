58adf6fc19d70a411474ca4361d64a325c7f5a69
Alisue <lambdalisue@gmail.com>
2022-03-04 01:58:00 +0900

    :package: Update dependencies

--- denops/gin/deps.ts
+++ denops/gin/deps.ts
@@ -1,18 +1,18 @@
 export { Cache } from "https://deno.land/x/local_cache@1.0/mod.ts";
 
-export type { Denops } from "https://deno.land/x/denops_std@v3.1.3/mod.ts";
-export * as anonymous from "https://deno.land/x/denops_std@v3.1.3/anonymous/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v3.1.3/autocmd/mod.ts";
-export * as batch from "https://deno.land/x/denops_std@v3.1.3/batch/mod.ts";
-export * as bufname from "https://deno.land/x/denops_std@v3.1.3/bufname/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v3.1.3/function/mod.ts";
-export * as helper from "https://deno.land/x/denops_std@v3.1.3/helper/mod.ts";
-export * as mapping from "https://deno.land/x/denops_std@v3.1.3/mapping/mod.ts";
-export * as option from "https://deno.land/x/denops_std@v3.1.3/option/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v3.1.3/variable/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v3.1.4/mod.ts";
+export * as anonymous from "https://deno.land/x/denops_std@v3.1.4/anonymous/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v3.1.4/autocmd/mod.ts";
+export * as batch from "https://deno.land/x/denops_std@v3.1.4/batch/mod.ts";
+export * as bufname from "https://deno.land/x/denops_std@v3.1.4/bufname/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v3.1.4/function/mod.ts";
+export * as helper from "https://deno.land/x/denops_std@v3.1.4/helper/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v3.1.4/mapping/mod.ts";
+export * as option from "https://deno.land/x/denops_std@v3.1.4/option/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v3.1.4/variable/mod.ts";
 
 export * as unknownutil from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
 
-export * as fs from "https://deno.land/std@0.126.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.126.0/path/mod.ts";
-export { deadline } from "https://deno.land/std@0.126.0/async/mod.ts";
+export * as fs from "https://deno.land/std@0.128.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.128.0/path/mod.ts";
+export { deadline } from "https://deno.land/std@0.128.0/async/mod.ts";
--- denops/gin/deps_test.ts
+++ denops/gin/deps_test.ts
@@ -1,8 +1,8 @@
-export * from "https://deno.land/std@0.126.0/testing/asserts.ts";
-export * as bytes from "https://deno.land/std@0.126.0/bytes/mod.ts";
+export * from "https://deno.land/std@0.128.0/testing/asserts.ts";
+export * as bytes from "https://deno.land/std@0.128.0/bytes/mod.ts";
 
 export { using } from "https://deno.land/x/disposable@v1.0.2/mod.ts";
 
 export { sandbox } from "https://deno.land/x/sandbox@v1.0.3/mod.ts";
 
-export * from "https://deno.land/x/denops_std@v3.1.3/test/mod.ts";
+export * from "https://deno.land/x/denops_std@v3.1.4/test/mod.ts";
