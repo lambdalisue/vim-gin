612353f85d0f3ca2313d7294d5d24e369e815d0b
Alisue <lambdalisue@gmail.com>
2022-03-13 22:14:14 +0900

    :+1: Use friendlyCall on raw git command

--- denops/gin/core/bare/main.ts
+++ denops/gin/core/bare/main.ts
@@ -1,12 +1,12 @@
-import { Denops, unknownutil } from "../../deps.ts";
+import { Denops, helper, unknownutil } from "../../deps.ts";
 import { command } from "./command.ts";
 
 export function main(denops: Denops): void {
   denops.dispatcher = {
     ...denops.dispatcher,
-    "command": (args: unknown) => {
+    command: (args: unknown) => {
       unknownutil.assertArray(args, unknownutil.isString);
-      return command(denops, args);
+      return helper.friendlyCall(denops, () => command(denops, args));
     },
   };
 }
