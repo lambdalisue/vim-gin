dbb9768ba8a7c187c0cd00696162e01cc7cbafb4
Alisue <lambdalisue@gmail.com>
2022-03-15 03:29:57 +0900

    :+1: Add `parseCommitish`

---
+++ denops/gin/feat/diff/commitish.ts
@@ -0,0 +1,34 @@
+export const INDEX = Symbol("INDEX");
+export const WORKTREE = Symbol("WORKTREE");
+
+export type Commitish = string | typeof INDEX | typeof WORKTREE;
+
+// git diff
+//  INDEX -> WORKTREE
+// git diff A
+//  A -> WORKTREE
+//
+// git diff --cached
+//  HEAD -> INDEX
+// git diff --cached A
+//  A -> INDEX
+//
+// git diff A..B
+//  A -> B
+// git diff A...B
+//  A...B -> B
+export function parseCommitish(
+  commitish: string,
+  cached = false,
+): [Commitish, Commitish] {
+  if (commitish.includes("...")) {
+    return [commitish, commitish.split("...")[1]];
+  } else if (commitish.includes("..")) {
+    const [a, b] = commitish.split("..", 2);
+    return [a, b];
+  } else if (cached) {
+    return [commitish, INDEX];
+  } else {
+    return [commitish || INDEX, WORKTREE];
+  }
+}
